045ea0bd981de9db6499975ec34d9de7ad08fb79
David Moraes <investtraderrs@gmail.com>
2020-09-20 17:24:02 -0300

    Exercício: Inicio da problemática

---
+++ Composicao/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Composicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*COMPOSIÇÃO: É um tipo de associação que permite que um objeto contenha outro
+             
+            relação "tem um" ou "tem-vários"
+
+            VANTAGENS:
+            - Organização: divisão de responsabilitades
+            - Coesão
+            - Flexibilidade
+            - Reuso
+
+            Nota: Embora o símbolo UML para composição (todo-parte) seja o diamante preto, neste contexto estamos chamando de composição qualquer associação do tipo "tem-um" e "tem-vários"             
+            */
+
+
+
+        }
+    }
+}
+/*Exercicio Exemplo: Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). 
+ Depois, solicitar do usuário um mês e mostrar qual foi o salário do funcionário nesse mês, conforme exemplo:
+Exemplo:
+
+Nome do departamento: Design
+
+Entre com os dados do trabalhador:
+Nome: Alex
+Nível (Junior/Intermediário/Senior: Intermediario.
+Salário Base: 1200.00
+
+Quantos contratos para esse trabalhador? 3
+
+Entre com os dados do #1 contrato:
+Data (DD/MM/YYYY): 20/08/2018
+Valor por hora: 50.00
+Duração (horas): 20
+
+Entre com os dados do #2 contrato:
+Data (DD/MM/YYYY): 13/06/2018
+Valor por hora: 30.00
+Duração (horas): 18
+
+Entre com os dados do #3 contrato:
+Data (DD/MM/YYYY): 25/08/2018
+Valor por hora: 80.00
+Duração (horas): 10
+
+Entre com o mês e o ano para calcular quanto o funcionário recebeu nesse mês e ano (MM/YYYY): 08/2018
+
+Nome: Alex
+Departamento: Design
+Ganhor para 08/2018: 3000.00
+
+ */
